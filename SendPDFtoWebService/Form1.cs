8e06bb7cecaa93eb5ef9383a85e5faa491e8e4d2
ycedillos <yonathan.cedillos@gmail.com>
2017-11-04 15:40:19 -0600

    Get HTTP Status Code

--- SendPDFtoWebService/Form1.cs
+++ SendPDFtoWebService/Form1.cs
@@ -9,7 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using SendPDFtoWebService;
 using System.IO;
-
+using System.Net;
+using System.Xml;
 
 namespace SendPDFtoWebService
 {
@@ -22,15 +23,43 @@ namespace SendPDFtoWebService
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SendPDFtoWebService.SendPdfToWebService.SendFaxGateway spdf = new SendPdfToWebService.SendFaxGateway();
-            
-           // object[] docBytes = new object[1];
-            var doc = File.ReadAllBytes(textBox2.Text);
-            
-            var res = spdf.CaptureFile(textBox1.Text, doc);
-            int x = 1;
+            using (SendPDFtoWebService.SendPdfToWebService.SendFaxGateway spdf = new SendPdfToWebService.SendFaxGateway())
+            {
+
+                // object[] docBytes = new object[1];
+
+                var doc = File.ReadAllBytes(textBox2.Text);
+
+                callWebService(textBox1.Text, Convert.ToBase64String(doc));
+                //spdf.CaptureFileAsync(textBox1.Text, doc,"");
+
+                int x = 1;
+            }
         }
 
+        
+
+
+        private void callWebService(string faxNumber, string doc)
+        {
+            using (VMExpertiseWebClient client = new VMExpertiseWebClient())
+            {
+                client.Headers.Add("SOAPAction", "\"http://tempuri.org/CaptureFile\"");
+                client.Headers.Add("Content-Type", "text/xml; charset=utf-8");
+                //client.Headers.Add("Host", "localhost");
+
+                var payload = "<?xml version=\"1.0\" encoding=\"utf-8\"?><soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\"><soap:Body><CaptureFile xmlns=\"http://tempuri.org/\"><faxNumber>"+faxNumber+"</faxNumber><DocumentContent>"+doc+"</DocumentContent></CaptureFile></soap:Body></soap:Envelope>";
+                var data = Encoding.UTF8.GetBytes(payload);
+                var result = client.UploadData("http://localhost:8087/SendFaxGateway.asmx", data);
+                if (client.StatusCode() == HttpStatusCode.OK)
+                {
+
+                }
+                                
+            }
+        }
+        
+        
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
