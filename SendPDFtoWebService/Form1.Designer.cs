603da4ace949c9ccf96c27f81a5f1a237078d148
pkirklewski <kirklewskis@gmail.com>
2017-11-07 11:01:46 +0000

    Working. After presentation to Amer and Mark. Need to add all the fields from the XSD.

--- SendPDFtoWebService/Form1.Designer.cs
+++ SendPDFtoWebService/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(11, 77);
+            this.button1.Location = new System.Drawing.Point(11, 310);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(260, 72);
             this.button1.TabIndex = 0;
@@ -45,7 +45,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(194, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(259, 20);
             this.textBox1.TabIndex = 1;
@@ -53,7 +53,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 48);
+            this.textBox2.Location = new System.Drawing.Point(193, 38);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(260, 20);
             this.textBox2.TabIndex = 2;
@@ -64,12 +64,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.ClientSize = new System.Drawing.Size(491, 394);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "CMG";
             this.ResumeLayout(false);
             this.PerformLayout();
 
